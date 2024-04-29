126f1c9fd555c6f1f9fcded162cd42cc067d0f3e
arifeld <24385099+arifeld@users.noreply.github.com>
2024-04-29 14:02:40 +1000

    init/add react-router-dom (badly)

--- client/main.jsx
+++ client/main.jsx
@@ -3,8 +3,33 @@ import { createRoot } from 'react-dom/client';
 import { Meteor } from 'meteor/meteor';
 import { App } from '/imports/ui/App';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import MovieList from '../imports/ui/MovieList';
+import { HomePage } from '../imports/ui/HomePage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    children: [
+      {
+        path: "movielist",
+        // this does not work
+        element: <MovieList />
+      },
+    
+    ]
+  }
+])
+
 Meteor.startup(() => {
   const container = document.getElementById('react-target');
   const root = createRoot(container);
-  root.render(<App />);
+  root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>);
 });
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import { HomePage } from './HomePage.jsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './HomePage.jsx';
+import { Outlet } from 'react-router-dom';
+// import Navbar from './Navbar.jsx';
+// import { Profile } from './Profile.jsx';
+// import { AIPicks } from './AIPicks.jsx';
+//import './App.css';
 
-export const App = () => (
-  <div>
-    <HomePage/>
-  </div>
-);
+
+
+export const App = () => {
+  return (
+    <div>
+      <p> Hello World. </p>
+      <Outlet />
+    </div>
+  )
+}
+
