d7c242b4b76ee6cbf77f4313638753a528814661
arifeld <24385099+arifeld@users.noreply.github.com>
2024-04-29 12:42:37 +1000

    feat/Add Tailwind configuration with package.json

--- imports/ui/HomePage.jsx
+++ imports/ui/HomePage.jsx
@@ -75,7 +75,7 @@ export const HomePage = () => {
 
   return (
     <div>
-      <h1>Welcome to Meteor!</h1>
+      <h1 className="text-3xl font-bold underline">Welcome to Meteor!</h1>
 
       {/* Add movie functionality */}
       <p>Add a new movie!</p>
