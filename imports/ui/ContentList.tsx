25d00617e16e102bbbf5136a8c69abf0ebca7280
lachlandurra <lochiedurra@icloud.com>
2024-04-30 11:13:27 +1000

    Making content sqaure

--- imports/ui/ContentList.tsx
+++ imports/ui/ContentList.tsx
@@ -1,18 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-
-
-  //how to import the images
-
-/*   const images = [
-    { src: './ExampleResources/sienfeld.jpg', alt: 'Seinfeld' },
-    { src: './ExampleResources/friends.jpg', alt: 'Friends' },
-    { src: './ExampleResources/007.jpg', alt: '007' },
-    { src: './ExampleResources/planet-earth.jpeg', alt: 'Planet Earth' },
-    { src: './ExampleResources/fresh-prince.jpg', alt: 'Fresh Prince' }
-  ]; */
-
 interface Image {
   src: string;
   alt: string;
@@ -33,18 +21,20 @@ const ContentList: React.FC<ContentListProps> = ({ title, images }) => {
   return (
     <div className="flex flex-col mt-12">
       <div className="flex justify-between items-center mb-5 text-base">
-        <h1 className="ml-2.5 font-bold text-3xl text-white-800 leading-tight tracking-tight sm:text-3xl">{title}</h1>
+        <h1 className="ml-2.5 font-bold text-3xl text-white leading-tight tracking-tight sm:text-3xl">{title}</h1>
         <button onClick={handleRedirect} className="mr-2.5 text-3xl bg-transparent border-none cursor-pointer">+</button>
       </div>
-      <div className="flex justify-around items-center flex-nowrap">
+      <div className="flex justify-start items-start flex-wrap gap-4">
         {images.map((image, index) => (
-          <img
-            key={index}
-            src={image.src}
-            alt={image.alt}
-            className="m-2.5 max-w-[175px] max-h-[100px] h-auto object-cover rounded-lg cursor-pointer hover:scale-105 transition-transform duration-200"
-            style={{ flex: '0 0 calc(16.66% - 20px)' }}
-          />
+          <div key={index} className="flex flex-col items-start">  
+            <img
+              src={image.src}
+              alt={image.alt}
+              className="transition-transform duration-200 ease-in-out transform hover:scale-110 cursor-pointer"
+              style={{ width: '160px', height: '160px', objectFit: 'cover', borderRadius: '8px' }}
+            />
+            <div className="text-white mt-2 text-left">{image.alt}</div>  
+          </div>
         ))}
       </div>
     </div>
