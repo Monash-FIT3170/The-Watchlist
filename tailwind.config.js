5872c156085eb6e939d271732656b68e303afc8e
stefan-ty-su <stefansu3526@gmail.com>
2024-05-03 11:35:18 +1000

    Added custom colours and box shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     "./client/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Configure your color palette here
+        'purplish-grey': "#B5A6AB",
+        'magenta': "#7B1450",
+        'dark-magenta': "#410227",
+        'off-white': "#E2DCDE",
+        'smoky-black': "14110F",
+        'dark': "#282525",
+        'darker': "#181818",
+        'darkest': "#080808",
+      },
+      boxShadow: {
+        'nav': '0 0px 2px rgba(255, 255, 255, 0.4)',
+        'nav-inner': 'inset 0 0px 4px rgba(255, 255, 255, 0.4)'
+      }
+    },
+    
   },
   plugins: [],
 }
