40d8893eb65b3d84f27f7754b165f8ca604753b9
lachlandurra <lochiedurra@icloud.com>
2024-05-03 11:26:02 +1000

    Updating App and Navbar to pass in the dummy lists, movies and tv shows

--- imports/ui/Navbar.tsx
+++ imports/ui/Navbar.tsx
@@ -37,7 +37,7 @@ export default function Navbar({ staticNavData, listData }) {
               <ul>
                 {listData.map((list) => (
                   <li key={list.id}>
-                    <Link to={`/${list.id}`}>{list.title}</Link>
+                    <Link to={`/${list.listId}`}>{list.title}</Link>
                   </li>
                 ))}
               </ul>
