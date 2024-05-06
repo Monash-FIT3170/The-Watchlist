c6908866ea33517391ce5c80fdad72b22fe21b9b
selsasony <sson0024@student.monash.edu>
2024-05-06 12:11:24 +1000

    added scaffold of movie info page

--- imports/ui/MovieInfo.tsx
+++ imports/ui/MovieInfo.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import ContentItem from './ContentItem';
 
 const MovieInfo = ({ movie }) => {
     return (
-        <div>
-            <h1 className="text-3xl font-bold">{movie.title}</h1>
-            <div className="flex flex-wrap justify-flex-start items-start mt-4">
+        <div className="flex flex-col items-center justify-end h-screen text-white bg-cover bg-center p-0" style={{ backgroundImage: `url(${movie.image_url})` }}>
+            <div className="w-full h-1/2 bg-black bg-opacity-1 flex flex-col items-center justify-center rounded-lg">
+                <img src={movie.image_url} alt={movie.title} className="w-48 h-auto rounded-lg" />
+                <h1 className="text-6xl font-bold text-center mt-5">{movie.title}</h1>
+                <p className="text-lg text-center max-w-xl mt-5">{movie.overview}</p>
+                <p className="text-lg text-purple-500 font-bold mt-5">Rating: {movie.rating}</p>
+                <div className="mt-10">
+                    <button className="px-5 py-2 mr-5 border-none rounded-lg cursor-pointer bg-white text-purple-500">+ My List</button>
+                </div>
             </div>
         </div>
     );
