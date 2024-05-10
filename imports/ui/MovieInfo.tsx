cc2ab4137fb1686851777fc67a4bce2aab1c7b13
Ishita <80767328+weavingandwandering@users.noreply.github.com>
2024-05-10 08:46:08 +0800

    Positioning of the inner div box and changing opacity of the background image

--- imports/ui/MovieInfo.tsx
+++ imports/ui/MovieInfo.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 
 const MovieInfo = ({ movie }) => {
     return (
-        <div className="flex flex-col items-center justify-end h-screen text-white bg-cover bg-center p-0" style={{ backgroundImage: `url(${movie.image_url})` }}>
-        <div className="w-full h-3/4 bg-black bg-opacity-1 rounded-xl shadow-md overflow-hidden md:max-w-4xl flex flex-col items-center justify-center">
-<img src={movie.image_url} alt={movie.title} className="w-48 h-auto rounded-lg" />
+        <div className="flex flex-col items-center justify-end h-screen text-white bg-cover bg-center p-0 relative">
+        <div className="absolute inset-0 w-full h-full bg-center bg-cover" style={{ backgroundImage: `url(${movie.image_url})`, opacity: 0.3 }}></div>
+        <div className="absolute inset-0 w-full h-full flex flex-col items-center justify-center">
+            <div className="w-full h-3/4 rounded-xl shadow-md overflow-hidden md:max-w-4xl bg-black bg-opacity-100 text-white flex flex-col items-center justify-center">
+                <img src={movie.image_url} alt={movie.title} className="w-48 h-auto rounded-lg" />
                 <h1 className="text-6xl font-bold text-center mt-5">{movie.title}</h1>
                 <p className="text-lg text-center max-w-xl mt-5">{movie.overview}</p>
                 <p className="text-lg text-purple-500 font-bold mt-5">Rating: {movie.rating}</p>
@@ -14,6 +16,7 @@ const MovieInfo = ({ movie }) => {
                 </div>
             </div>
         </div>
+    </div>
     );
 };
 
