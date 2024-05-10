e50b1d8ddaf160e8fed4ee4e4d8e47b844e2c0ad
stefan-ty-su <stefansu3526@gmail.com>
2024-05-10 11:11:09 +1000

    Changed which dummy data is being printed

--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Favourites from './Favourites';
 import ToWatch from './ToWatch';
-import dummyMovies from './DummyMovies';
+import dummyLists from './DummyLists';
 
 const HomePage = () => {
   const [favourites, setFavourites] = useState([]);
@@ -12,10 +12,10 @@ const HomePage = () => {
   return (
     <div className="flex justify-between space-x-5">
       <div className="w-1/2 p-5 bg-darker text-light rounded-lg shadow-md h-custom flex justify-center">
-        <Favourites dummyMovies={dummyMovies} />
+        <Favourites dummyMovies={dummyLists} />
       </div>
       <div className="w-1/2 p-5 bg-darker text-light rounded-lg shadow-md h-custom flex justify-center">
-        <ToWatch dummyMovies={dummyMovies} />
+        <ToWatch dummyMovies={dummyLists} />
       </div>
     </div>
   );
