f2ec567d8b7ba0e15edfa49c4f7f54b67b612428
arifeld <24385099+arifeld@users.noreply.github.com>
2024-05-16 22:16:02 +1000

    Further refactor ingestor to remove unneeded .data reference for each call

--- server/APIIngestor.js
+++ server/APIIngestor.js
@@ -18,7 +18,7 @@ class APIIngestor {
      * @param {string} url The relative URL to retrieve, including the preceding "/"
      * @param {string} type The type of request, usually either "GET" or "POST" 
      * @param {object|null} data The data to send alongside a POST/PUT/DELETE request, if needed. 
-     * @returns A Promise that will resolve with the HTTP request data, available via .data
+     * @returns The data result from the fetch request (i.e. the expected output from the API, with all other Axios fields removed).
      */
     async fetch(url, type="GET", data=null) {
         const config = {
@@ -34,23 +34,25 @@ class APIIngestor {
             config["headers"] = { "Authorization": `Bearer ${this.#bearerToken}` }
         }
 
-        return this.#instance.request(config)
-            .catch((error) => {
-                if (error.response) {
-                    console.log(error.response.data);
-                    console.log(error.response.status);
-                    console.log(error.response.headers);
-                } else if (error.request) {
-                    console.log(error.request);
-                }
-                else {
-                    console.log(`Unknown error: ${error.message}`);
-                }
+        data = await this.#instance.request(config)
+        .catch((error) => {
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            }
+            else {
+                console.log(`Unknown error: ${error.message}`);
+            }
 
-                console.log(error.config);
-            });
+            console.log(error.config);
+        });
+
+
+        return data.data;
     }
-
     /**
      * Authenticates against the API using the TVDB_KEY environment variable
      */
@@ -59,33 +61,36 @@ class APIIngestor {
             apikey: process.env.TVDB_KEY
         })
         
-        this.#bearerToken = data.data["data"]["token"];
+        this.#bearerToken = data["data"]["token"];
         console.log("Connected to API successfully.");        
     };
 
 
     async getAllMovies() {
         let page = 0;
-        let next = "/movies?page=0";
+        let next = "/movies?page=0&limit=20";
 
         while (next != null) {
-            currentData = await this.fetch(next).data;
+            console.log(`Retrieving URL: ${next}`)
+            let currentData = await this.fetch(next);
+            console.log(currentData.data);
 
-            for (movie in currentData.data) {
-
+            for (const movie of currentData.data) {
+                console.log(movie);
                 const movieData = {
                     id: movie.id,
                     title: movie.name,
                     release_year: parseInt(movie.year),
                     image_url: movie.image,
                     runtime: movie.runtime,
-
+                    rating: movie.score
                 };
 
-                translationData = await this.fetch(`/movies/${movie.id}/translations/eng`).data;
-                console.log(translationData);
-                // translationData.data.overview
-            
+                console.log(`Getting extended data for movie: ${movie.id}`)
+                // Get the extended movie data, which also includes the overviews.
+                let extendedData = await this.fetch(`/movies/${movie.id}/extended?meta=translations&short=true`);
+
+                console.log(extendedData);
             }
 
         
@@ -101,7 +106,6 @@ class APIIngestor {
 const ingestor = new APIIngestor();
 ingestor.authenticate()
     .then(async () => {
-        data = await ingestor.fetch("/movies");
-        console.log(data.data);
+        data = ingestor.getAllMovies();
     })
 
