845dd68f09983594dbb71ac7807d5db199137b5d
lucaswee626 <lucaswee626@gmail.com>
2024-05-17 00:40:30 +1000

    feat/Update UserDiscovery component and add UserList component

--- imports/ui/UserDiscovery.jsx
+++ imports/ui/UserDiscovery.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React from 'react';
+import UserList from './UserList';
 
 const UserDiscovery = () => {
   return (
-    <div className="user-discovery">
-      <h1>User Discovery</h1>
+    <div className="user-discovery text-center ">
+      <h1 className="text-white text-3xl p-8 mb-6 bg-darker rounded-lg shadow-md">User Discovery</h1>
+      <div >
+        <UserList heading="Popular Users" />
+        <div className="mt-8">
+          <UserList heading="Similar Users" />
+        </div>
+      </div>
     </div>
   );
-}
+};
+
 export default UserDiscovery;
