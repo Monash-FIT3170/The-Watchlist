40718b566eae1cb48b20451080ba9c047c099cfd
Ishita <80767328+weavingandwandering@users.noreply.github.com>
2024-05-17 11:13:44 +0800

    removed redundant code

--- imports/ui/TvInfo.tsx
+++ imports/ui/TvInfo.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import dummyTVs from './DummyTvs';
 
-interface TvInfoProps {
-    id: number;
-    title: string;
-    overview: string;
-    image_url: string;
-    first_aired: Date;
-    last_aired: Date;
-    seasons: string[];
-}
 
 const TvInfo = ({ tv }) => {
     return (
         <div className="flex flex-col items-center justify-end h-screen text-white bg-cover bg-center p-0 relative">
             {/* Background overlay */}
-            <div className="absolute inset-0 w-full h-full bg-cover bg-center opacity-30" style={{ backgroundImage: `url(${tv.image_url})` }}></div>
-            
+            <div className="absolute inset-0 w-full h-full bg-cover bg-center" style={{ backgroundImage: `url(${tv.image_url})`, filter: "blur(9px)" }}></div>
             {/* Main content */}
             <div className="absolute inset-0 w-full h-full flex flex-col items-center justify-center">
-            <div className=" min-h-3/4 rounded-xl shadow-md md:max-w-4xl overflow-y-auto py-6 bg-black bg-opacity-100 text-white flex flex-row items-center justify-between px-5 h-auto w-full">
+            <div className="min-h-3/4 rounded-xl shadow-lg md:max-w-3xl overflow-y-auto py-6 bg-black bg-opacity-100 text-white flex flex-row items-center justify-between px-5 h-auto w-full" style={{ boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.4)' }}>
                     {/* Left column */}
                     <div className="flex flex-col items-start w-2/5 gap-4">
                         <img src={tv.image_url} alt={tv.title} className="w-full h-auto rounded-lg mb-5 ml-6" />
@@ -28,7 +17,7 @@ const TvInfo = ({ tv }) => {
                     
                     {/* Right column */}
                     <div className="flex flex-col items-start w-1/2">
-                    <h1 className="font-bold text-center text-5xl max-w-full overflow-hidden  whitespace-wrap">{tv.title}</h1>
+                    <h1 className="font-bold font-famil text-center text-5xl max-w-full overflow-hidden  whitespace-wrap">{tv.title}</h1>
                     <h6 className="text-2xl font-bold text-center max-w-full overflow-hidden whitespace-nowrap">({tv.first_aired.getFullYear().toString()} - {tv.last_aired.getFullYear().toString()})</h6>
 
                         <p className="text-2xl font-semibold text-center mt-5">SYNOPSIS</p>
