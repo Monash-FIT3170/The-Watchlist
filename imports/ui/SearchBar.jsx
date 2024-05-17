fefe904fa7f15aeb2575e558ba3607347c7e92c1
lachlandurra <lochiedurra@icloud.com>
2024-05-18 00:23:15 +1000

    Fixing redirect of tv shows

--- imports/ui/SearchBar.jsx
+++ imports/ui/SearchBar.jsx
@@ -273,14 +273,14 @@ const SearchBar = ({ movies, tvs, lists }) => {
                 {selectedTab === 'movies' && (
                     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
                         {filteredData.movies.length > 0 ? filteredData.movies.map(movie => (
-                            <ContentItem key={movie.id} id={movie.id} type="movie" src={movie.image_url} alt={movie.title} rating={movie.rating} />
+                            <ContentItem key={movie.id} id={movie.id} type="Movie" src={movie.image_url} alt={movie.title} rating={movie.rating} />
                         )) : <div>No movies available.</div>}
                     </div>
                 )}
                 {selectedTab === 'tv shows' && (
                     <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
                         {filteredData.tvShows.length > 0 ? filteredData.tvShows.map(tv => (
-                            <ContentItem key={tv.id} id={tv.id} type="tv" src={tv.image_url} alt={tv.title} rating={tv.rating || undefined} />
+                            <ContentItem key={tv.id} id={tv.id} type="TV Show" src={tv.image_url} alt={tv.title} rating={tv.rating || undefined} />
                         )) : <div>No TV shows available.</div>}
                     </div>
                 )}
