bd6513f5715dbf27b218df47fdded905d1ea03fe
Ishita <80767328+weavingandwandering@users.noreply.github.com>
2024-05-17 14:57:34 +0800

    UI for showing up all the lists

--- imports/ui/Modal.tsx
+++ imports/ui/Modal.tsx
@@ -5,7 +5,8 @@ const Modal = ({ show, onClose }) => {
 
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-            <div className="bg-white rounded-lg shadow-lg p-8 max-w-md w-full relative">
+            <div className="bg-white rounded-lg shadow-lg p-8 max-w-lg h-80 w-full relative"> 
+
                 <button onClick={onClose} className="absolute top-2 right-2 text-black text-xl">×</button>
                 <button className="px-5 py-2 bg-purple-500 text-white rounded-lg" onClick={onClose}>
                     Add to list
---
+++ imports/ui/ViewLists.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useNavigate } from 'react-router-dom';
+import RatingStar from './RatingStar';
+
+const List = ({ list }) => {
+    const navigate = useNavigate();
+
+    // Function to handle redirection
+    const handleRedirect = (type, id) => {
+        navigate(`/${type}${id}`);
+    };
+
+    return (
+        <div key={list._id} className="space-y-8">
+            {list.content.map((item) => (
+                <div key={item.id} className="relative" onClick={() => handleRedirect(item.type, item.content_id)}>
+                    <div className="relative rounded-lg shadow-lg cursor-pointer overflow-visible">
+                        <div className="transition-transform duration-300 ease-in-out transform hover:scale-110">
+                            <img
+                                src={item.image_url}
+                                alt={item.title}
+                                className="w-full h-35vh object-cover rounded-lg"
+                            />
+                            <div className="absolute inset-0 bg-black bg-opacity-50 flex items-end p-4 rounded-lg transition-opacity duration-300 ease-in-out hover:bg-opacity-60">
+                                <div className="text-white">
+                                    <h3 className="text-xl font-bold">{item.title}</h3>
+                                    <p className="text-sm">{item.description}</p>
+                                    <RatingStar totalStars={5} rating={item.rating} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+const HomeList = ({ lists, title, listType }) => {
+    const filteredMovies = lists.filter(list => list.listType === listType);
+
+    return (
+        <div className="w-full h-full px-5 py-5 rounded-lg flex flex-col items-left shadow-xl overflow-auto scrollbar-thumb-gray-900 scrollbar-track-gray-100 scrollbar-thin">
+            <h1 className="font-sans font-bold text-4xl my-4 mt-0 mb-4">{title}</h1>
+            <div className="w-full overflow-visible">
+                {filteredMovies.map((list) => <List list={list} />)}
+            </div>
+        </div>
+    );
+};
+
+export default HomeList;
