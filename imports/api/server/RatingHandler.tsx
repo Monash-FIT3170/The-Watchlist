f9c8305e7de49e9ee021700ca4d082ff68c6dbf6
arifeld <24385099+arifeld@users.noreply.github.com>
2024-05-19 14:07:18 +1000

    Add backend for rating, including live publishing

---
+++ imports/api/server/RatingHandler.tsx
@@ -0,0 +1,41 @@
+import { Handler, HandlerFunc } from './Handler';
+
+import { Rating } from '../../db/Rating';
+import { Movie, TV } from '../../db/Content';
+
+export type AddRatingOptions = {
+    content_type: "TV" | "Movie",
+    content_id: Number,
+    rating: 1 | 2 | 3 | 4 | 5
+}
+
+const createRating: HandlerFunc = {
+    validate: null,
+    run: ({content_type, content_id, rating}: AddRatingOptions) => {
+
+        // Check that the content with the specified ID exists
+        if (content_type == "TV" && TV.findOne({id: content_id}) == undefined) {
+            throw new Error(`TV series with ID ${content_id} does not exist!`);
+        }
+
+        else if (content_type == "Movie" && Movie.findOne({id: content_id}) == undefined) {
+            throw new Error(`Movie with ID ${content_id} does not exist!`);
+        }
+        
+        Rating.insert({
+            content_type,
+            content_id,
+            rating
+        });
+    }
+}
+
+const RatingHandler = new Handler("rating")
+    .addCreateHandler(createRating);
+
+export default RatingHandler;
+
+// We will also publish the dataset so that clients can get the updated ratings live
+Meteor.publish("rating", function publicRating() {
+    return Rating.find({});
+})
