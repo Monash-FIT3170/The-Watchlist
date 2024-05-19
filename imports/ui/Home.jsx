0aff7908ebefc9b1c697ace8f0fe9965411a6e3a
stefan-ty-su <stefansu3526@gmail.com>
2024-05-19 21:16:48 +1000

    Removed redundant scrollbar on homepage

--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -5,10 +5,10 @@ import ToWatch from './ToWatch';
 const Home = ({ lists }) => {
   return (
     <div className="flex justify-between space-x-5 overflow-hidden">
-      <div className="w-1/2 h-screen p-5 bg-darker text-light rounded-lg shadow-md flex justify-center overflow-hidden">
+      <div className="w-1/2 h-custom p-5 bg-darker text-light rounded-lg shadow-md flex justify-center overflow-hidden">
         <Favourites lists={lists} />
       </div>
-      <div className="w-1/2 h-screen p-5 bg-darker text-light rounded-lg shadow-md flex justify-center overflow-hidden">
+      <div className="w-1/2 h-custom p-5 bg-darker text-light rounded-lg shadow-md flex justify-center overflow-hidden">
         <ToWatch lists={lists} />
       </div>
     </div>
