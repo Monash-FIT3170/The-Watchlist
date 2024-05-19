3897e5003152b9803b411f4a7b65b9d853bbb05a
madsters <madeleineneaves@hotmail.com>
2024-05-19 13:39:58 +1000

    UI done

--- imports/ui/MovieInfo.jsx
+++ imports/ui/MovieInfo.jsx
@@ -1,9 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from './Modal'; 
+import { Rating } from '@mui/material';
 
 const MovieInfo = ({ movie, initialLists }) => { 
     const [showModal, setShowModal] = useState(false);
     const [lists, setLists] = useState(initialLists);
+    const [value, setValue] = useState(null);
+
+    useEffect(() => {
+        if (0 < movie.rating && movie.rating < 6) {
+            setValue(movie.rating);
+            console.log(movie.rating);
+        }
+    })
+
+    const handleRating = (newValue) => {
+        console.log(movie);
+        movie.rating = newValue;
+
+    }
 
     const handleAddContent = (listId, content) => {
         const userId = 1; // Temporary userId: 1
@@ -37,12 +52,24 @@ const MovieInfo = ({ movie, initialLists }) => {
                 <div className="w-full h-3/4 rounded-xl shadow-md overflow-hidden md:max-w-4xl bg-black bg-opacity-90 text-white flex flex-row items-center justify-between px-5">
                     <div className="flex flex-col items-start w-2/5 gap-4">
                         <img src={movie.image_url} alt={movie.title} className="w-full h-auto rounded-lg mb-5 ml-6" />
-                        <button 
-                            className="px-5 py-2 border-none rounded-lg cursor-pointer bg-white text-purple-500 mb-5 ml-6"
-                            onClick={() => setShowModal(true)}
-                        >
-                            +
-                        </button>
+                        <div className="grid grid-cols-2 grid-rows-1 gap-1">
+                            <Rating
+                                name="simple-controlled"
+                                value={value}
+                                onChange={(event, newValue) => {
+                                    setValue(newValue);
+                                    handleRating(newValue);
+                                }}
+                            />
+                            <button 
+                                className="px-5 py-2 border-none rounded-lg cursor-pointer bg-white text-purple-500 mb-5 ml-6"
+                                onClick={() => setShowModal(true)}
+                            >
+                                +
+                            </button>
+
+                        </div>
+                        
                     </div>
                     <div className="flex flex-col items-start w-1/2">
                         <h1 className="text-6xl font-bold text-center">{movie.title} ({movie.release_year})</h1>
