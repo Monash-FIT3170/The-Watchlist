2118975fc117c9cbc26d56e4030deaa4df3af9dd
arifeld <24385099+arifeld@users.noreply.github.com>
2024-07-29 12:22:09 +1000

    Update ingestion to use JSON stream, add language and country fields

--- data-ingestion/data-ingestion.py
+++ data-ingestion/data-ingestion.py
@@ -5,6 +5,7 @@ import os
 import requests
 from datetime import datetime
 from dotenv import load_dotenv
+from json_stream.writer import streamable_list 
 
 ROOT_URL = "https://api.themoviedb.org/3"
 IMAGE_ROOT = "http://image.tmdb.org/t/p/original"
@@ -36,12 +37,27 @@ def make_request(partial_url):
 
 
 
-def process_all_movies():
-    movie_file = open("movie_ids_07_28_2024.json", encoding='utf8')
-    movie_output = open("all_movies.json", "w", encoding="utf8")
+@streamable_list
+def process_movies():
+    '''
+    Processes a particular movie from the initial data, yielding the result.
+    This approach is used to make the data streamable so that RAM does not get filled.
+    '''
 
+    with open("movie_ids_07_28_2024.json", "rb") as f:
+        num_lines = sum(1 for _ in f)
+
+    movie_file = open("movie_ids_07_28_2024.json", encoding='utf8')
+
+    
+    i = 0
     # Read each line and parse it as JSON (each line of the file is valid JSON, the entire file is not)
     for movie_line in movie_file:
+        i += 1
+        print(f"[{i}/{num_lines}]")
+        
+        if i == 100: break
+ 
         initial_data = json.loads(movie_line)
         #print(initial_data)
 
@@ -52,7 +68,7 @@ def process_all_movies():
         }
 
         # Retrieve the movie details and artwork via one request using the append_to_response query parameter
-        request_url = f"/movie/{initial_data.get('id')}?append_to_response=images&language=en-US&include_image_language=en,null"
+        request_url = f"/movie/{initial_data.get('id')}?append_to_response=keywords,credits,images&language=en-US&include_image_language=en,null"
 
         data = make_request(request_url)
 
@@ -60,8 +76,12 @@ def process_all_movies():
         movie_data["runtime"] = data.get("runtime")
         movie_data["popularity"] = data.get("popularity")
         movie_data["overview"] = data.get("overview")
+        movie_data["language"] = data.get("original_language")
+        movie_data["origin_country"] = data.get("origin_country")        
+        movie_data["genres"] = [genre["name"] for genre in data.get("genres")]
+
+        movie_data["keywords"] = [keyword["name"] for keyword in data.get("keywords").get("keywords")]
         
-        movie_data["genres"] = [genre["name"] for genre in data.get('genres')]
         movie_data["background_url"] = ""
         movie_data["image_url"] = ""
 
@@ -73,14 +93,18 @@ def process_all_movies():
             if images.get("posters") is not None and len(images.get("backdrops")):
                 movie_data["image_url"] = IMAGE_ROOT + images.get("posters")[0]["file_path"]
         
-
-        print(movie_data)
-
-        # Save the line to file
-
-
-
+        yield movie_data
+    
     movie_file.close()
+
+
+def process_all_movies():
+    
+    movie_output = open("all_movies.json", "w", encoding="utf8")
+    all_data = process_movies()
+    
+    json.dump(all_data, movie_output, indent=4)
+    
     movie_output.close()
         
 
