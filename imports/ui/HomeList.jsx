fd079b89e8e1c08b9a8c5b62d7d8f6cb78706396
lachlandurra <lochiedurra@icloud.com>
2024-07-29 18:24:14 +1000

    Fixing empty list issue

--- imports/ui/HomeList.jsx
+++ imports/ui/HomeList.jsx
@@ -76,14 +76,19 @@ const List = ({ list }) => {
 };
 
 const HomeList = ({ title, listType }) => {
-    const { lists } = useLists();
+    const { lists, loading } = useLists();
     const navigate = useNavigate();
     const filteredLists = lists.filter(list => list.listType === listType);
+
+    // Check if all lists have empty content
+    const isEmpty = filteredLists.every(list => list.content.length === 0);
 
     return (
         <div className="w-full h-full px-5 py-5 rounded-lg flex flex-col items-left shadow-xl overflow-auto scrollbar-thumb-gray-900 scrollbar-track-gray-100 scrollbar-thin">
             <h1 className="font-sans font-bold text-4xl my-4 mt-0 mb-4">{title}</h1>
-            {filteredLists.length === 0 ? (
+            {loading ? (
+                <div>Loading...</div> // Display loading message
+            ) : isEmpty ? (
                 <div className="flex flex-col items-center justify-center flex-grow">
                     <p className="text-lg text-white mb-4">You don't have any items in this list yet.</p>
                     <button 
@@ -101,5 +106,6 @@ const HomeList = ({ title, listType }) => {
         </div>
     );
 };
+
 
 export default HomeList;
