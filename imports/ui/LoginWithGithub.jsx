adfabd35579a865aa1b9bb2d18854670ab3e949d
lucaswee626 <lucaswee626@gmail.com>
2024-07-31 15:39:53 +1000

    change github login back to popup

--- imports/ui/LoginWithGithub.jsx
+++ imports/ui/LoginWithGithub.jsx
@@ -5,7 +5,7 @@ export const LoginWithGithub = () => {
   const handleGithubLogin = () => {
     Meteor.loginWithGithub({
       requestPermissions: ['user'],
-      loginStyle: 'redirect',
+      loginStyle: 'popup',
     });
   };
 
