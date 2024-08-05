a2c82e97584b2d001b73815e18585274fbc5568e
stefan-ty-su <stefansu3526@gmail.com>
2024-08-05 14:00:02 +1000

    Reformatted to use HomeList components instead. Better and more efficient design

--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import Favourites from './Favourites';
-import ToWatch from './ToWatch';
+import HomeList from './HomeList'
 
 const Home = ({ lists }) => {
   return (
     <div className="flex justify-between space-x-5 overflow-hidden">
       <div className="w-1/2 h-custom p-5 bg-darker text-light rounded-lg shadow-md flex justify-center overflow-hidden">
-        <Favourites lists={lists} />
+        <HomeList lists={lists} title="Favourites" listType="Favourite" />
       </div>
       <div className="w-1/2 h-custom p-5 bg-darker text-light rounded-lg shadow-md flex justify-center overflow-hidden">
-        <ToWatch lists={lists} />
+        <HomeList lists={lists} title="To Watch" listType="To Watch" />
       </div>
     </div>
   );
