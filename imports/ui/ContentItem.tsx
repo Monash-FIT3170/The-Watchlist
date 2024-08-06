74f58704842b3f6947830a0216da41cb52d71d6f
lachlandurra <lochiedurra@icloud.com>
2024-08-06 18:27:14 +1000

    Major changes to Content Item to bring up modal rather than new page

--- imports/ui/ContentItem.tsx
+++ imports/ui/ContentItem.tsx
@@ -1,35 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ContentInfoModal from './ContentInfoModal'; // Ensure this import is correct
+import TvInfo from './TvInfo';
 import RatingStar from "./RatingStar";
-import { FaGlobeAsia , FaUser } from "react-icons/fa";
-import { useNavigate } from 'react-router-dom';
+import { FaGlobe, FaUser } from "react-icons/fa";
+import { getImageUrl } from './imageUtils';
 
-const ContentItem = ({ id, type, src, alt, rating, isUserSpecificRating }) => {
-    const navigate = useNavigate();
+const ContentItem = ({ content, isUserSpecificRating }) => {
 
-    const handleRedirect = () => {
-        navigate(`/${type}${id}`);
+    const [isOpen, setOpen] = useState(false);
+
+    const toggleModal = () => {
+        setOpen(!isOpen);
     };
+
+    // Determine which modal to use based on the content type
+    // const ModalComponent = content.type === 'Movie' ? ContentInfoModal : TvInfoModal;
+    const ModalComponent = ContentInfoModal;
 
     return (
         <div className="flex flex-col items-start mr-4 w-40">
-            <button onClick={handleRedirect}>
-                <img src={src} alt={alt} className="transition-transform duration-200 ease-in-out transform hover:scale-110 cursor-pointer" style={{ width: '160px', height: 'auto', borderRadius: '8px', margin: '8px' }} />
+            <button onClick={toggleModal}>
+                <img src={content.image_url} alt={content.title} className="transition-transform duration-200 ease-in-out transform hover:scale-110 cursor-pointer" style={{ width: '160px', height: 'auto', borderRadius: '8px', margin: '8px' }} />
             </button>
-            <div className="text-white mt-0 ml-2 text-left" style={{ fontWeight: 'normal', height: '72px', overflow: 'hidden' }}>{alt}</div>
+            {/* Conditional rendering of the modal */}
+            {isOpen && <ModalComponent content={content} isOpen={isOpen} onClose={toggleModal} />}
+            <div className="text-white mt-0 ml-2 text-left" style={{
+                fontWeight: 'normal',
+                height: '48px',
+                overflow: 'hidden',
+                display: '-webkit-box',
+                WebkitLineClamp: '2',
+                WebkitBoxOrient: 'vertical',
+                textOverflow: 'ellipsis'
+            }}>{content.title}</div>
             <div className="flex items-center ml-2 mt-1">
-                {isUserSpecificRating ? <FaUser className="mr-1 text-blue-500" /> : <FaGlobeAsia  className="mr-1 text-green-500" />}
-                <RatingStar totalStars={5} rating={rating} />
+                {isUserSpecificRating ? <FaUser className="mr-1 text-blue-500" /> : <FaGlobe className="mr-1 text-green-500" />}
+                <RatingStar totalStars={5} rating={content.rating} />
             </div>
-        </div>
-    );
-};
-
-const ContentList = ({ items }) => {
-    return (
-        <div className="grid grid-cols-4 gap-4">
-            {items.map(item => (
-                <ContentItem key={item.id} {...item} />
-            ))}
         </div>
     );
 };
