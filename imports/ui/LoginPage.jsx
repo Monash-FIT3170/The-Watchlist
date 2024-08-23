28ec5b0f74345efd7319c1053fc427d36c6bb9b0
lucaswee626 <lucaswee626@gmail.com>
2024-08-23 15:30:09 +1000

    improve login screen background

--- imports/ui/LoginPage.jsx
+++ imports/ui/LoginPage.jsx
@@ -52,16 +52,18 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center min-h-screen py-40"
+    <div
+      className="flex flex-col items-center justify-center min-h-screen w-full"
       style={{
         backgroundImage: `url(/test.jpg)`,
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
-        backgroundPosition: '0px -250px'}}
+        backgroundPosition: 'center',
+      }}
     >
       <h1 className="text-3xl mb-4 text-white font-bold">The Watchlist</h1>
       <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center w-full max-w-xs bg-darker/90 px-5 py-7 rounded-2xl">
-        
+
         <h1 className="text-white font-bold text-3xl mb-7 mt-2">
           {isRegistering ? 'Sign Up' : 'Login'}
         </h1>
@@ -100,23 +102,23 @@ const LoginPage = () => {
         </button>
 
         <p className="text-white text-xs font-thin">
-          {isRegistering ? "Have an account?":"Don't have an account?"}
+          {isRegistering ? "Have an account?" : "Don't have an account?"}
           <a
             className="font-bold"
             href="#" onClick={() => setIsRegistering(!isRegistering)}>
             {isRegistering ? " Login!" : " Sign Up!"}
           </a>
         </p>
-        
+
         <div className='flex flex-row w-full items-center mt-8 mb-5'>
-          <hr className='w-full h-px bg-white'/>
+          <hr className='w-full h-px bg-white' />
           <p className=' text-white px-1 text-sm'>or</p>
-          <hr className='w-full h-px bg-white'/>
+          <hr className='w-full h-px bg-white' />
         </div>
 
         <div className="flex flex-col w-full py-1 items-center">
           <div className="w-3/4 mb-5">
-            <LoginWithGithub/>
+            <LoginWithGithub />
           </div>
           <div className="w-3/4">
             <LoginWithGoogle />
