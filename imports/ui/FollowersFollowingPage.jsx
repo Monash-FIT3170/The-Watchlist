404e51e5fb2ec0caf4cfc8317fd7a675cc544679
SinethFer <141303264+SinethFer@users.noreply.github.com>
2024-08-23 17:54:33 +1000

    Fixed bug - re-fetching users immediately

--- imports/ui/FollowersFollowingPage.jsx
+++ imports/ui/FollowersFollowingPage.jsx
@@ -3,27 +3,28 @@ import { useParams } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
 import AllUsersPage from './AllUsersPage';
 
-const FollowersFollowingPage = ( { currentUser } ) => {
+const FollowersFollowingPage = ({ currentUser }) => {
   const { userId, type } = useParams(); // Get userId and type (followers/following) from URL parameters
   const [usersList, setUsersList] = useState([]);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const users = await new Promise((resolve, reject) => {
-          Meteor.call(`users.${type}`, { userId }, (error, result) => {
-            if (error) {
-              reject(error);
-            } else {
-              resolve(result);
-            }
-          });
+  const fetchUsers = async () => {
+    try {
+      const users = await new Promise((resolve, reject) => {
+        Meteor.call(`users.${type}`, { userId }, (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(result);
+          }
         });
-        setUsersList(users);
-      } catch (error) {
-        console.error(`Error fetching ${type}:`, error);
-      }
-    };
+      });
+      setUsersList(users);
+    } catch (error) {
+      console.error(`Error fetching ${type}:`, error);
+    }
+  };
+
+  useEffect(() => {
     fetchUsers();
   }, [userId, type]);
 
@@ -31,6 +32,7 @@ const FollowersFollowingPage = ( { currentUser } ) => {
     <AllUsersPage 
       currentUser={currentUser}
       users={usersList}
+      fetchUsers={fetchUsers} // Pass the fetchUsers function to AllUsersPage
     />
   );
 };
