d2a1d7c6eff17dd56f328e860d07e5b32f11ad74
mrud0004 <mrud0004@student.monash.edu>
2024-09-09 12:13:51 +1000

    Cleaned up CSS for user setting page.
    Fixed up some css bugs with the user setting page and made it look neater.

--- imports/ui/pages/UserSettingsPage.jsx
+++ imports/ui/pages/UserSettingsPage.jsx
@@ -39,34 +39,32 @@ const UserSettingsPage= () => {
     <div classname = "flex flex-col items-center">
         <h1 className = "ml-4 text-4xl text-white font-bold mb-2">Profile Visibility</h1>
         <div className = "ml-4 mt-2">
-        <Select name="status"className={clsx(
-              'mt-5 block w-1/3 appearance-none rounded-lg border-none bg-white/5 py-1.5 px-3 text-md text-white',
+        <Select name="privacy"className={clsx(
+              'mt-10 block w-80 appearance-none rounded-lg border-none bg-less-dark py-1.5 px-3 text-md text-white',
               'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
             )} aria-label="Project status">
-      <option value="active">Public</option>
-      <option value="paused">Private</option>
+      <option value="public">Public</option>
+      <option value="private">Private</option>
     </Select>
     </div>
     <button
-          className="mt-4 ml-72 text-md text-white py-2 px-6 rounded-full shadow bg-less-dark hover:bg-darker"
+          className="mb-1 mt-8 ml-64 text-md text-white py-2 px-6 rounded-full shadow bg-less-dark hover:bg-darker"
         >
           Save
         </button>
 
    
-        <h1 className = "ml-4 mt-3 text-4xl text-white font-bold mb-4">Profile Photo</h1>
-        <div className = "flex flex-row">
+        <h1 className = "ml-4 mt-8 text-4xl text-white font-bold mb-4">Profile Photo</h1>
+        <div className = "flex flex-row mt-8">
         <img
             src={ currentUser?.avatarUrl || "https://randomuser.me/api/portraits/lego/1.jpg"}
             alt="avatar"
-            className="ml-5 aspect-square w-32 h-32 object-cover rounded-full shadow-2xl transition-opacity duration-300 ease-in-out"
+            className="ml-8 aspect-square w-32 h-32 object-cover rounded-full shadow-2xl transition-opacity duration-300 ease-in-out"
 
             style={{ zIndex: 10 }} // Ensures the image is always clickable
           />
-        <button className="h-12  mt-10 ml-10 text-white bg-less-dark  hover:bg-darker rounded-full py-2 px-6">Change Photo</button>
-        </div>
-        <h1 className = "mt-5 ml-4 text-4xl text-white font-bold mb-2">Change Username</h1>
-        
+        <button className="h-12  mt-10 ml-24 text-white bg-less-dark  hover:bg-darker rounded-full py-2 px-6">Change Photo</button>
+        </div>        
         </div>
     </>
   );
