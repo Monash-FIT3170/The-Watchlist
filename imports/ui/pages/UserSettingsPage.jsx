66d3b9724a8fda4c0c2be56550ae15c2cd8111b6
Aydin <thefickers@outlook.com>
2024-09-11 13:24:29 +1000

    user can now change their name within the settings page

--- imports/ui/pages/UserSettingsPage.jsx
+++ imports/ui/pages/UserSettingsPage.jsx
@@ -1,74 +1,123 @@
-import React, { Fragment, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useTracker } from 'meteor/react-meteor-data';
-import ProfileCard from '../components/headers/ProfileCard';
-import ContentList from '../components/lists/ContentList';
-import { ListCollection } from '../../db/List';
-import ListDisplay from '../components/lists/ListDisplay';
-import ProfileDropdown from '../components/profileDropdown/ProfileDropdown';
-import { Select } from '@headlessui/react'
-import clsx from 'clsx'
+import React, { Fragment, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useTracker } from "meteor/react-meteor-data";
+import ProfileCard from "../components/headers/ProfileCard";
+import ContentList from "../components/lists/ContentList";
+import { ListCollection } from "../../db/List";
+import ListDisplay from "../components/lists/ListDisplay";
+import ProfileDropdown from "../components/profileDropdown/ProfileDropdown";
+import { Select } from "@headlessui/react";
+import clsx from "clsx";
 
-
-const UserSettingsPage= () => {
-
-    const { userId } = useParams();
-    const [userLists, setUserLists] = useState([]);
-    const [globalRatings, setGlobalRatings] = useState({});
+const UserSettingsPage = () => {
+  const { userId } = useParams();
+  const [userLists, setUserLists] = useState([]);
+  const [globalRatings, setGlobalRatings] = useState({});
+  const [newUsername, setNewUsername] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [successMessage, setSuccessMessage] = useState(""); // State for success messages
 
   const currentUser = useTracker(() => {
-    const handler = Meteor.subscribe('userData', Meteor.userId());
+    const handler = Meteor.subscribe("userData", Meteor.userId());
     if (handler.ready()) {
       return Meteor.user();
     }
     return null;
   }, []);
 
+  const handleUsernameChange = (e) => {
+    setNewUsername(e.target.value);
+  };
+
+  const handleSaveUsername = () => {
+    Meteor.call("updateUsername", newUsername, (error) => {
+      if (error) {
+        setUsernameError(error.reason);
+      } else {
+        setUsernameError("");
+        setSuccessMessage("Username updated successfully!");
+        setNewUsername(""); // Optionally reset the input field
+      }
+    });
+  };
+
   return (
     <>
-    <div className="flex flex-col justify-end items-start w-full h-52 p-4 bg-gradient-to-tl from-zinc-900 via-zinc-700 to-zinc-600 rounded-t-lg shadow-md mb-4">
-      <div className="absolute top-4 right-6">
-        <ProfileDropdown user={currentUser} />
-      </div>
-      <h1 className="text-7xl text-white font-bold mb-2">User Settings</h1>
+      <div className="flex flex-col justify-end items-start w-full h-52 p-4 bg-gradient-to-tl from-zinc-900 via-zinc-700 to-zinc-600 rounded-t-lg shadow-md mb-4">
+        <div className="absolute top-4 right-6">
+          <ProfileDropdown user={currentUser} />
+        </div>
+        <h1 className="text-7xl text-white font-bold mb-2">User Settings</h1>
 
-      
-      <div className="flex gap-4 mb-4">
+        <div className="flex gap-4 mb-4"></div>
       </div>
-    </div>
-    <div classname = "flex flex-col items-center">
-        <h1 className = "ml-4 text-4xl text-white font-bold mb-2">Profile Visibility</h1>
-        <div className = "ml-4 mt-2">
-        <Select name="privacy"className={clsx(
-              'mt-10 block w-80 appearance-none rounded-lg border-none bg-less-dark py-1.5 px-3 text-md text-white',
-              'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
-            )} aria-label="Project status">
-      <option value="public">Public</option>
-      <option value="private">Private</option>
-    </Select>
-    </div>
-    <button
-          className="mb-1 mt-8 ml-64 text-md text-white py-2 px-6 rounded-full shadow bg-less-dark hover:bg-darker"
-        >
+      <div classname="flex flex-col items-center">
+        <h1 className="ml-4 text-4xl text-white font-bold mb-2">
+          Profile Visibility
+        </h1>
+        <div className="ml-4 mt-2">
+          <Select
+            name="privacy"
+            className={clsx(
+              "mt-10 block w-80 appearance-none rounded-lg border-none bg-less-dark py-1.5 px-3 text-md text-white",
+              "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25"
+            )}
+            aria-label="Project status"
+          >
+            <option value="public">Public</option>
+            <option value="private">Private</option>
+          </Select>
+        </div>
+        <button className="mb-1 mt-8 ml-64 text-md text-white py-2 px-6 rounded-full shadow bg-less-dark hover:bg-darker">
           Save
         </button>
 
-   
-        <h1 className = "ml-4 mt-8 text-4xl text-white font-bold mb-4">Profile Photo</h1>
-        <div className = "flex flex-row mt-8">
-        <img
-            src={ currentUser?.avatarUrl || "https://randomuser.me/api/portraits/lego/1.jpg"}
+        <h1 className="ml-4 mt-8 text-4xl text-white font-bold mb-2">
+          Change Username
+        </h1>
+        <div className="ml-4 mt-2">
+          <input
+            type="text"
+            value={newUsername}
+            onChange={handleUsernameChange}
+            placeholder="Enter new username"
+            className="text-input mt-2 p-2 rounded-lg text-white w-80 shadow bg-less-dark hover:bg-darker "
+          />
+          {usernameError && (
+            <p className="text-red-500 mt-2">{usernameError}</p>
+          )}
+          {successMessage && (
+            <p className="text-green-500 mt-2">{successMessage}</p>
+          )}
+        </div>
+
+        <button
+          className="mb-1 mt-8 text-md text-white py-2 px-6 rounded-full shadow bg-less-dark hover:bg-darker"
+          onClick={handleSaveUsername}
+        >
+          Save Username
+        </button>
+
+        <h1 className="ml-4 mt-8 text-4xl text-white font-bold mb-4">
+          Profile Photo
+        </h1>
+        <div className="flex flex-row mt-8">
+          <img
+            src={
+              currentUser?.avatarUrl ||
+              "https://randomuser.me/api/portraits/lego/1.jpg"
+            }
             alt="avatar"
             className="ml-8 aspect-square w-32 h-32 object-cover rounded-full shadow-2xl transition-opacity duration-300 ease-in-out"
-
             style={{ zIndex: 10 }} // Ensures the image is always clickable
           />
-        <button className="h-12  mt-10 ml-24 text-white bg-less-dark  hover:bg-darker rounded-full py-2 px-6">Change Photo</button>
-        </div>        
+          <button className="h-12  mt-10 ml-24 text-white bg-less-dark  hover:bg-darker rounded-full py-2 px-6">
+            Change Photo
+          </button>
         </div>
+      </div>
     </>
   );
 };
-
 
 export default UserSettingsPage;
--- server/methods.js
+++ server/methods.js
@@ -1,55 +1,74 @@
-import { Meteor } from 'meteor/meteor';
-import { check } from 'meteor/check';
-import { Accounts } from 'meteor/accounts-base';
-import { Rating } from '../imports/db/Rating';
-import { RatingCollection } from '../imports/db/Rating';
-import Lists from '../imports/db/List';
-import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
-import { ListCollection } from '../imports/db/List';
-import { MovieCollection, TVCollection } from '../imports/db/Content';
+import { Meteor } from "meteor/meteor";
+import { check } from "meteor/check";
+import { Accounts } from "meteor/accounts-base";
+import { Rating } from "../imports/db/Rating";
+import { RatingCollection } from "../imports/db/Rating";
+import Lists from "../imports/db/List";
+import { FlowRouter } from "meteor/ostrio:flow-router-extra";
+import { ListCollection } from "../imports/db/List";
+import { MovieCollection, TVCollection } from "../imports/db/Content";
 
 Meteor.methods({
   followUser(targetUserId) {
     check(targetUserId, String);
     if (!this.userId) {
-      throw new Meteor.Error('not-authorized');
+      throw new Meteor.Error("not-authorized");
     }
-    Meteor.users.update(this.userId, { $addToSet: { following: targetUserId } });
-    Meteor.users.update(targetUserId, { $addToSet: { followers: this.userId } });
+    Meteor.users.update(this.userId, {
+      $addToSet: { following: targetUserId },
+    });
+    Meteor.users.update(targetUserId, {
+      $addToSet: { followers: this.userId },
+    });
   },
   unfollowUser(targetUserId) {
     check(targetUserId, String);
     if (!this.userId) {
-      throw new Meteor.Error('not-authorized');
+      throw new Meteor.Error("not-authorized");
     }
     Meteor.users.update(this.userId, { $pull: { following: targetUserId } });
     Meteor.users.update(targetUserId, { $pull: { followers: this.userId } });
   },
   isFollowing(targetUserId) {
     if (!this.userId) {
-      throw new Meteor.Error('not-authorized');
+      throw new Meteor.Error("not-authorized");
     }
     const user = Meteor.users.findOne(this.userId);
     return user.following.includes(targetUserId);
   },
-  'users.followers'({ userId }) {
+  "users.followers"({ userId }) {
     check(userId, String);
     const user = Meteor.users.findOne(userId);
     if (!user) {
-      throw new Meteor.Error('not-found', 'User not found');
+      throw new Meteor.Error("not-found", "User not found");
     }
-    return Meteor.users.find({ _id: { $in: user.followers } }, { fields: { username: 1, avatarUrl: 1 } }).fetch();
+    return Meteor.users
+      .find(
+        { _id: { $in: user.followers } },
+        { fields: { username: 1, avatarUrl: 1 } }
+      )
+      .fetch();
   },
-  'users.following'({ userId }) {
+  "users.following"({ userId }) {
     check(userId, String);
     const user = Meteor.users.findOne(userId);
     if (!user) {
-      throw new Meteor.Error('not-found', 'User not found');
+      throw new Meteor.Error("not-found", "User not found");
     }
-    return Meteor.users.find({ _id: { $in: user.following } }, { fields: { username: 1, avatarUrl: 1 } }).fetch();
+    return Meteor.users
+      .find(
+        { _id: { $in: user.following } },
+        { fields: { username: 1, avatarUrl: 1 } }
+      )
+      .fetch();
   },
-  'ratings.addOrUpdate'({ userId, contentId, contentType, rating }) {
-    console.log('addOrUpdate method called with:', { userId, contentId, contentType, rating });
+  "ratings.addOrUpdate"({ userId, contentId, contentType, rating }) {
+    console.log("addOrUpdate method called with:", {
+      userId,
+      contentId,
+      contentType,
+      rating,
+    });
 
     check(userId, String);
     check(contentId, Number);
@@ -57,20 +76,23 @@ Meteor.methods({
     check(rating, Number);
 
     if (!this.userId) {
-      throw new Meteor.Error('not-authorized');
+      throw new Meteor.Error("not-authorized");
     }
 
     const existingRating = Rating.findOne({ userId, contentId, contentType });
     if (existingRating) {
-      Rating.update({ _id: existingRating._id }, {
-        $set: { rating }
-      });
+      Rating.update(
+        { _id: existingRating._id },
+        {
+          $set: { rating },
+        }
+      );
     } else {
       Rating.insert({
         userId: this.userId,
         contentId,
         contentType,
-        rating
+        rating,
       });
     }
   },
@@ -80,35 +102,40 @@ Meteor.methods({
     check(avatarUrl, String);
 
     if (!this.userId) {
-      throw new Meteor.Error('not-authorized');
+      throw new Meteor.Error("not-authorized");
     }
 
     if (this.userId !== userId) {
-      throw new Meteor.Error('not-authorized', 'You can only update your own avatar');
+      throw new Meteor.Error(
+        "not-authorized",
+        "You can only update your own avatar"
+      );
     }
 
     Meteor.users.update(userId, {
-      $set: { avatarUrl: avatarUrl }
+      $set: { avatarUrl: avatarUrl },
     });
   },
 
   // add method here
-
-
 });
 
 const createDefaultLists = (userId) => {
   const defaultLists = [
-    { title: 'Favourite', listType: 'Favourite' },
-    { title: 'To Watch', listType: 'To Watch' },
+    { title: "Favourite", listType: "Favourite" },
+    { title: "To Watch", listType: "To Watch" },
   ];
 
   defaultLists.forEach((list) => {
-    Meteor.call('list.create', { userId, title: list.title, listType: list.listType, content: [] }, (error) => {
-      if (error) {
-        console.error('Error creating default list:', error.reason);
+    Meteor.call(
+      "list.create",
+      { userId, title: list.title, listType: list.listType, content: [] },
+      (error) => {
+        if (error) {
+          console.error("Error creating default list:", error.reason);
+        }
       }
-    });
+    );
   });
 };
 
@@ -150,35 +177,40 @@ Accounts.onLogin(() => {
     // Update user document to indicate that default lists have been created
     Meteor.users.update(userId, {
       $set: {
-        defaultListsCreated: true
-      }
+        defaultListsCreated: true,
+      },
     });
   }
-  FlowRouter.go('/home');
+  FlowRouter.go("/home");
 });
 
-
 Meteor.methods({
-  'users.ratingsCount'({ userId }) {
+  "users.ratingsCount"({ userId }) {
     check(userId, String);
     return RatingCollection.find({ userId }).count();
   },
 });
 
 Meteor.methods({
-  'list.subscribe'(listId) {
+  "list.subscribe"(listId) {
     if (!this.userId) {
-      throw new Meteor.Error('not-authorized', 'You must be logged in to subscribe to lists.');
+      throw new Meteor.Error(
+        "not-authorized",
+        "You must be logged in to subscribe to lists."
+      );
     }
 
     const list = ListCollection.findOne({ _id: listId });
 
     if (!list) {
-      throw new Meteor.Error('not-found', 'List not found.');
+      throw new Meteor.Error("not-found", "List not found.");
     }
 
     if (list.userId === this.userId) {
-      throw new Meteor.Error('invalid-action', 'You cannot subscribe to your own list.');
+      throw new Meteor.Error(
+        "invalid-action",
+        "You cannot subscribe to your own list."
+      );
     }
 
     const result = ListCollection.update(
@@ -187,57 +219,71 @@ Meteor.methods({
     );
 
     if (result) {
-      return 'Subscribed successfully';
+      return "Subscribed successfully";
     } else {
-      throw new Meteor.Error('failed', 'Failed to subscribe to list.');
+      throw new Meteor.Error("failed", "Failed to subscribe to list.");
     }
-  }
+  },
 });
-
 
 // Server-side method to fetch subscribed lists
 Meteor.methods({
-  'list.getSubscribed': function ({ userId }) {
+  "list.getSubscribed": function ({ userId }) {
     // Check if the userId is provided
     if (!userId) {
-      throw new Meteor.Error('invalid-argument', 'You must provide a user ID.');
+      throw new Meteor.Error("invalid-argument", "You must provide a user ID.");
     }
 
     // Check if the user calling the method is logged in
     if (!this.userId) {
-      throw new Meteor.Error('not-authorized', 'You must be logged in to access subscribed lists.');
+      throw new Meteor.Error(
+        "not-authorized",
+        "You must be logged in to access subscribed lists."
+      );
     }
 
     // Check if the user is requesting their own subscribed lists
     if (this.userId !== userId) {
-      throw new Meteor.Error('not-authorized', 'You are not authorized to view other users\' subscribed lists.');
+      throw new Meteor.Error(
+        "not-authorized",
+        "You are not authorized to view other users' subscribed lists."
+      );
     }
 
     // Fetch and return lists where the specified userId is in the subscribers array
-    return Lists.find({
-      subscribers: { $in: [userId] }
-    }, {
-      fields: { title: 1, userName: 1, content: 1 }  // Limit the fields returned for privacy/security
-    }).fetch();
-  }
+    return Lists.find(
+      {
+        subscribers: { $in: [userId] },
+      },
+      {
+        fields: { title: 1, userName: 1, content: 1 }, // Limit the fields returned for privacy/security
+      }
+    ).fetch();
+  },
 });
 
 Meteor.methods({
-  'list.unsubscribe'(listId) {
+  "list.unsubscribe"(listId) {
     check(listId, String);
 
     if (!this.userId) {
-      throw new Meteor.Error('not-authorized', 'You must be logged in to unsubscribe from lists.');
+      throw new Meteor.Error(
+        "not-authorized",
+        "You must be logged in to unsubscribe from lists."
+      );
     }
 
     const list = ListCollection.findOne({ _id: listId });
 
     if (!list) {
-      throw new Meteor.Error('not-found', 'List not found.');
+      throw new Meteor.Error("not-found", "List not found.");
     }
 
     if (list.userId === this.userId) {
-      throw new Meteor.Error('invalid-action', 'You cannot unsubscribe from your own list.');
+      throw new Meteor.Error(
+        "invalid-action",
+        "You cannot unsubscribe from your own list."
+      );
     }
 
     const result = ListCollection.update(
@@ -246,15 +292,15 @@ Meteor.methods({
     );
 
     if (result) {
-      return 'Unsubscribed successfully';
+      return "Unsubscribed successfully";
     } else {
-      throw new Meteor.Error('failed', 'Failed to unsubscribe from list.');
+      throw new Meteor.Error("failed", "Failed to unsubscribe from list.");
     }
-  }
+  },
 });
 
 Meteor.methods({
-  'ratings.getAverage'({ contentId, contentType }) {
+  "ratings.getAverage"({ contentId, contentType }) {
     check(contentId, Number);
     check(contentType, String);
 
@@ -266,7 +312,10 @@ Meteor.methods({
     }
 
     // Calculate the average rating
-    const totalRatings = ratings.reduce((sum, rating) => sum + rating.rating, 0);
+    const totalRatings = ratings.reduce(
+      (sum, rating) => sum + rating.rating,
+      0
+    );
     const averageRating = totalRatings / ratings.length;
 
     return averageRating;
@@ -274,7 +323,7 @@ Meteor.methods({
 });
 
 Meteor.methods({
-  'ratings.getUserRating'({ userId, contentId, contentType }) {
+  "ratings.getUserRating"({ userId, contentId, contentType }) {
     check(userId, String);
     check(contentId, Number);
     check(contentType, String);
@@ -286,24 +335,26 @@ Meteor.methods({
   },
 });
 Meteor.methods({
-  'genres.getAll': async function () {
-    const movieGenres = await MovieCollection.rawCollection().distinct("genres");
+  "genres.getAll": async function () {
+    const movieGenres = await MovieCollection.rawCollection().distinct(
+      "genres"
+    );
     const tvGenres = await TVCollection.rawCollection().distinct("genres");
 
     // Combine and deduplicate the genres
     const allGenres = Array.from(new Set([...movieGenres, ...tvGenres]));
 
     return allGenres;
-  }
+  },
 });
 Meteor.methods({
-  'ratings.getGlobalAverages'() {
+  "ratings.getGlobalAverages"() {
     const ratings = RatingCollection.find().fetch();
     const ratingMap = ratings.reduce((acc, rating) => {
       if (!acc[rating.contentId]) {
         acc[rating.contentId] = {
           count: 0,
-          total: 0
+          total: 0,
         };
       }
       acc[rating.contentId].count += 1;
@@ -312,50 +363,61 @@ Meteor.methods({
     }, {});
 
     for (const id in ratingMap) {
-      ratingMap[id].average = (ratingMap[id].total / ratingMap[id].count).toFixed(2);
+      ratingMap[id].average = (
+        ratingMap[id].total / ratingMap[id].count
+      ).toFixed(2);
     }
 
     return ratingMap;
-  }
+  },
 });
 Meteor.methods({
-  'users.getSimilarUsers'() {
+  "users.getSimilarUsers"() {
     if (!this.userId) {
-      throw new Meteor.Error('not-authorized', 'You must be logged in to find similar users.');
+      throw new Meteor.Error(
+        "not-authorized",
+        "You must be logged in to find similar users."
+      );
     }
 
     const currentUser = Meteor.users.findOne(this.userId);
     if (!currentUser) {
-      throw new Meteor.Error('user-not-found', 'User not found.');
+      throw new Meteor.Error("user-not-found", "User not found.");
     }
 
     const currentUserFavourites = ListCollection.findOne({
       userId: this.userId,
-      listType: 'Favourite'
+      listType: "Favourite",
     });
 
     if (!currentUserFavourites || currentUserFavourites.content.length === 0) {
-      throw new Meteor.Error('no-favourites', 'You do not have any favourites.');
+      throw new Meteor.Error(
+        "no-favourites",
+        "You do not have any favourites."
+      );
     }
 
     const currentUserFavouritesSet = new Set(currentUserFavourites.content);
 
-    const randomUsers = Meteor.users.rawCollection().aggregate([
-      { $sample: { size: 100 } }
-    ]).toArray();
+    const randomUsers = Meteor.users
+      .rawCollection()
+      .aggregate([{ $sample: { size: 100 } }])
+      .toArray();
 
-    const randomUsersPromise = Meteor.wrapAsync((cb) => randomUsers.then((res) => cb(null, res)).catch(cb));
+    const randomUsersPromise = Meteor.wrapAsync((cb) =>
+      randomUsers.then((res) => cb(null, res)).catch(cb)
+    );
 
     const users = randomUsersPromise();
 
-    const userScores = users.map(user => {
+    const userScores = users.map((user) => {
       if (user._id === this.userId) {
-        return null; 
+        return null;
       }
 
       const userFavourites = ListCollection.findOne({
         userId: user._id,
-        listType: 'Favourite'
+        listType: "Favourite",
       });
 
       if (!userFavourites || !userFavourites.content) {
@@ -363,21 +425,25 @@ Meteor.methods({
       }
 
       const userFavouritesSet = new Set(userFavourites.content);
-      
+
       const userFavouritesArray = Array.from(userFavouritesSet);
       const currentUserFavouritesArray = Array.from(currentUserFavouritesSet);
 
-      const commonFavourites = userFavouritesArray.filter(favourite =>
-        currentUserFavouritesArray.some(currentFavourite => currentFavourite.contentId === favourite.contentId)
+      const commonFavourites = userFavouritesArray.filter((favourite) =>
+        currentUserFavouritesArray.some(
+          (currentFavourite) =>
+            currentFavourite.contentId === favourite.contentId
+        )
       );
 
       if (commonFavourites.size === 0) {
         return null;
-      } 
+      }
 
-      const avgListLength = (userFavouritesArray.length + currentUserFavouritesArray.length) / 2;
+      const avgListLength =
+        (userFavouritesArray.length + currentUserFavouritesArray.length) / 2;
 
-      matchScore = commonFavourites.length/avgListLength * 100 * 1.5;
+      matchScore = (commonFavourites.length / avgListLength) * 100 * 1.5;
 
       if (matchScore > 100) {
         matchScore = 100;
@@ -385,12 +451,14 @@ Meteor.methods({
 
       return {
         user: user,
-        matchScore: matchScore
+        matchScore: matchScore,
       };
     });
 
     // Filter out null values and sort by matchScore in descending order
-    const sortedUserScores = userScores.filter(Boolean).sort((a, b) => b.matchScore - a.matchScore);
+    const sortedUserScores = userScores
+      .filter(Boolean)
+      .sort((a, b) => b.matchScore - a.matchScore);
 
     // set top user as 100 match, etc
     sortedUserScores.forEach((user, index) => {
@@ -399,27 +467,62 @@ Meteor.methods({
 
     // Return the top 10 users with the highest match score
     return sortedUserScores.slice(0, 10);
-  }
+  },
 });
 Meteor.methods({
-  'list.getById': function (listId) {
+  "list.getById": function (listId) {
     console.log(`list.getById method called with listId: ${listId}`); // Log method call
 
     check(listId, String);
 
     if (!this.userId) {
-      console.log('User not authorized to view lists'); // Log unauthorized access
-      throw new Meteor.Error('not-authorized', 'You must be logged in to view lists.');
+      console.log("User not authorized to view lists"); // Log unauthorized access
+      throw new Meteor.Error(
+        "not-authorized",
+        "You must be logged in to view lists."
+      );
     }
 
     const list = ListCollection.findOne({ _id: listId });
 
     if (!list) {
       console.log(`List not found for listId: ${listId}`); // Log if list is not found
-      throw new Meteor.Error('not-found', 'List not found.');
+      throw new Meteor.Error("not-found", "List not found.");
     }
 
     return list;
   },
 });
 
+Meteor.methods({
+  updateUsername(newUsername) {
+    check(newUsername, String);
+
+    if (!this.userId) {
+      throw new Meteor.Error(
+        "not-authorized",
+        "You must be logged in to change your username."
+      );
+    }
+
+    if (newUsername.length < 3) {
+      throw new Meteor.Error(
+        "invalid-username",
+        "Username must be at least 3 characters long."
+      );
+    }
+
+    // Ensure the new username is unique
+    const existingUser = Meteor.users.findOne({ username: newUsername });
+    if (existingUser) {
+      throw new Meteor.Error(
+        "username-taken",
+        "This username is already taken."
+      );
+    }
+
+    Meteor.users.update(this.userId, {
+      $set: { username: newUsername },
+    });
+  },
+});
