6a26c493b987eb9e64ec8e4de9c2f4c77de8297a
Geomher Vergara <gver0007@student.monash.edu>
2024-09-15 09:16:09 +1000

    Add tooltips to share buttons

--- imports/ui/components/lists/ListPopup.jsx
+++ imports/ui/components/lists/ListPopup.jsx
@@ -313,15 +313,22 @@ const ListPopup = ({ listId, onClose, onRenameList }) => {
                         {/* <FacebookShareButton url={shareUrl} quote={shareQuote}>
                             <FacebookIcon size={iconSize} round />
                         </FacebookShareButton> */}
-                        <TwitterShareButton url={shareUrl} title={shareQuote}>
-                            <TwitterIcon size={iconSize} round />
-                        </TwitterShareButton>
-                        <WhatsappShareButton url={shareUrl} title={shareQuote}>
-                            <WhatsappIcon size={iconSize} round />
-                        </WhatsappShareButton>
-                        <EmailShareButton url={shareUrl} subject={list.title} body={shareQuote}>
-                            <EmailIcon size={iconSize} round />
-                        </EmailShareButton>
+                        <button title="Share to Twitter">
+                            <TwitterShareButton url={shareUrl} title={shareQuote}>
+                                <TwitterIcon size={iconSize} round />
+                            </TwitterShareButton>
+                        </button>
+                        <button title="Share to Whatsapp">
+                            <WhatsappShareButton url={shareUrl} title={shareQuote}>
+                                <WhatsappIcon size={iconSize} round />
+                            </WhatsappShareButton>
+                        </button>
+                        <button title="Send in Email">
+                            <EmailShareButton url={shareUrl} subject={list.title} body={shareQuote}>
+                                <EmailIcon size={iconSize} round />
+                            </EmailShareButton>
+                        </button>
+
                         <button
                             onClick={isCurrentUserList ? handleRenameListClick : null}
                             disabled={!isCurrentUserList}
