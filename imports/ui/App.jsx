30b0156645bef41d5f642f03242e37a6574848c1
Geomher Vergara <gver0007@student.monash.edu>
2024-09-15 08:14:19 +1000

    Created SharedWatchlist Page and SharedWatchlistHeader

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -21,6 +21,7 @@ import AllUsersPage from "./pages/AllUsersPage.jsx";
 import AllRatedContentPage from "./pages/AllRatedContentPage.jsx";
 import Loading from "./pages/Loading.jsx";
 import { ListCollection } from '../db/List.tsx';
+import SharedWatchlistPage from "./pages/SharedWatchlistPage.jsx";
 
 export const App = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -84,6 +85,7 @@ export const App = () => {
               <Route path="/" element={currentUser ? <Navigate to="/home" /> : <Navigate to="/login" />} />
               <Route path="/all-users" element={<AllUsersPage currentUser={currentUser} />} />
               <Route path="/user/:userId/ratings" element={<AllRatedContentPage currentUser={currentUser} />} />
+              <Route path="/watchlist/:listId" element={<SharedWatchlistPage/>} />
             </Routes>
           </Scrollbar>
         ) : (
