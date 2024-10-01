5b55d67fa5643e886a5fb31b4bd08b2417dc0b68
lachlandurra <lochiedurra@icloud.com>
2024-10-01 18:59:26 +1000

    adding shadow

--- imports/ui/components/carousel/Carousel.jsx
+++ imports/ui/components/carousel/Carousel.jsx
@@ -28,6 +28,7 @@ const Carousel = ({ items }) => {
 
   const handleClick = (index) => {
     setSelectedIndex(index);
+    setHoveredIndex(null); // Reset hover state when selecting an item
   };
 
   const handleFilterChange = (newFilter) => {
@@ -38,49 +39,53 @@ const Carousel = ({ items }) => {
 
   const selectedItem = filteredItems[selectedIndex];
 
-  const toggleModal = () => {
-    if (!fullContent) {
-      // Fetch full content details
-      Meteor.call(
-        'content.read',
-        { id: selectedItem.contentId, contentType: selectedItem.contentType },
-        (error, result) => {
-          if (!error && result.content?.length > 0) {
-            setFullContent(result.content[0]);
-            setIsOpen(true);
-          } else {
-            console.error('Error fetching content details:', error);
-          }
+  const openModal = () => {
+    // Always fetch full content details when opening the modal
+    Meteor.call(
+      'content.read',
+      { id: selectedItem.contentId, contentType: selectedItem.contentType },
+      (error, result) => {
+        if (!error && result.content?.length > 0) {
+          setFullContent(result.content[0]);
+          setIsOpen(true);
+        } else {
+          console.error('Error fetching content details:', error);
         }
-      );
-    } else {
-      setIsOpen(!isOpen);
-    }
+      }
+    );
   };
+
+  const closeModal = () => {
+    setIsOpen(false);
+    setFullContent(null); // Reset fullContent when closing the modal
+  };
+
 
   const isAnyHovered = hoveredIndex !== null;
 
   return (
     <div className="relative w-full text-white z-10">
       {/* Toggle for Filters */}
-      <div className="flex justify-center p-4">
-        <button
-          className={`mx-2 px-4 py-2 rounded ${
-            filter === 'Movies' ? 'bg-red-600' : 'bg-gray-700'
-          }`}
-          onClick={() => handleFilterChange('Movies')}
-        >
-          Movies
-        </button>
-        <button
-          className={`mx-2 px-4 py-2 rounded ${
-            filter === 'TV Shows' ? 'bg-red-600' : 'bg-gray-700'
-          }`}
-          onClick={() => handleFilterChange('TV Shows')}
-        >
-          TV Shows
-        </button>
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl font-bold text-white text-left">Trending</h1>
+        <div className="flex justify-left w-full ml-4">
+          <button
+            className={`text-lg text-white py-2 px-6 rounded-full shadow mr-4 ${filter === 'Movies' ? 'bg-[#7B1450]' : 'bg-gray-700'
+              }`}
+            onClick={() => handleFilterChange('Movies')}
+          >
+            Movies
+          </button>
+          <button
+            className={`text-lg text-white py-2 px-6 rounded-full shadow ${filter === 'TV Shows' ? 'bg-[#7B1450]' : 'bg-gray-700'
+              }`}
+            onClick={() => handleFilterChange('TV Shows')}
+          >
+            TV Shows
+          </button>
+        </div>
       </div>
+
 
       {/* Carousel Container */}
       <div className="flex overflow-hidden">
@@ -103,13 +108,13 @@ const Carousel = ({ items }) => {
               <div key={index} className="flex w-full">
                 {/* Left Panel */}
                 <div className="w-1/3 flex flex-col items-center justify-center bg-gradient-to-r from-black via-transparent to-transparent">
-                  <div className="text-9xl font-bold text-red-600">
+                  <div className="text-9xl font-bold text-[#7B1450]">
                     {index + 1}
                   </div>
                   {/* 'See More' button */}
                   <button
-                    className="mt-4 px-6 py-2 bg-red-600 rounded"
-                    onClick={toggleModal}
+                    className="mt-4 px-6 py-2 bg-[#7B1450] rounded"
+                    onClick={openModal}
                   >
                     See More
                   </button>
@@ -154,6 +159,10 @@ const Carousel = ({ items }) => {
                   alt={item.title}
                   className="w-full h-full object-cover"
                 />
+                {/* Overlay Shadow */}
+                {!isHovered && (
+                  <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50"></div>
+                )}
                 {/* Title at the bottom when hovered */}
                 {isHovered && (
                   <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-center py-2">
@@ -171,7 +180,7 @@ const Carousel = ({ items }) => {
         <ContentInfoModal
           content={fullContent}
           isOpen={isOpen}
-          onClose={toggleModal}
+          onClose={closeModal}
           onRatingUpdate={() => {
             // Handle rating updates if necessary
           }}
