97725bd512540a788068bbc9263da4294753ee15
mrud0004 <mrud0004@student.monash.edu>
2024-10-03 16:17:57 +1000

    Update loading screen for user profile + AI

--- imports/ui/pages/LoadingNoAnimation.jsx
+++ imports/ui/pages/LoadingNoAnimation.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { GiPopcorn } from 'react-icons/gi'
 
-const LoadingNoAnimation = ({ pageName, pageDesc}) => {
+const LoadingNoAnimation = ({ pageName, pageDesc, upperScreen}) => {
     
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-dark text-purplish-grey">
-      {/* Popcorn Icon */}
+    <div 
+    className={`flex flex-col items-center ${
+      upperScreen ? 'justify-start  pt-36' : 'justify-center'
+    } h-screen bg-dark text-purplish-grey`}
+  >
+      
       <div className="flex justify-center mb-4">
         <GiPopcorn className="text-9xl text-yellow-500 animate-jiggle" />
       </div>
 
-      {/* Title: The Watchlist */}
+
       <p className="text-8xl font-bold font-anton text-shadow-custom bg-gradient-to-r from-magenta to-magenta bg-clip-text text-transparent mb-4">
         The Watchlist
       </p>
 
-      {/* Description: Welcome to the show */}
+
       <p className="text-2xl font-bold font-anton text-center">
         {pageDesc}
       </p>
