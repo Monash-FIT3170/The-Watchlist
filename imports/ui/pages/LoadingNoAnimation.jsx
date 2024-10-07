6c88eee4bab6e05fb7e19949c7ad06d955e84c7a
mrud0004 <mrud0004@student.monash.edu>
2024-10-07 11:06:12 +1100

    Update loading animation to  handle case when no content found.

--- imports/ui/pages/LoadingNoAnimation.jsx
+++ imports/ui/pages/LoadingNoAnimation.jsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import { GiPopcorn } from 'react-icons/gi'
+import React, { useState, useEffect } from 'react';
+import { GiPopcorn } from 'react-icons/gi';
 
-const LoadingNoAnimation = ({ pageName, pageDesc, upperScreen}) => {
-    
+const LoadingNoAnimation = ({ pageName, pageDesc, upperScreen }) => {
+  const [description, setDescription] = useState(pageDesc);
+  const [failLoad, setFailedLoad] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDescription('No content found');
+      setFailedLoad(true)
+    }, 15000);
+
+    return () => clearTimeout(timer); 
+  }, []);
+
+  
 
   return (
     <div 
-    className={`flex flex-col items-center ${
-      upperScreen ? 'justify-start  pt-36' : 'justify-center'
-    } h-screen bg-darker text-purplish-grey`}
-  >
-      
+      className={`flex flex-col items-center ${
+        upperScreen ? 'justify-start pt-36' : 'justify-center'
+      } h-screen bg-darker text-purplish-grey`}
+    >
       <div className="flex justify-center mb-4 mt-14">
-        <GiPopcorn className="text-9xl text-yellow-500 animate-jiggle" />
+        <GiPopcorn className={`text-9xl text-yellow-500 ${ !failLoad ? 'animate-jiggle': ''}`}/>
       </div>
-
-
       <p className="text-2xl font-bold font-anton text-center">
-        {pageDesc}
+        {description}
       </p>
     </div>
   );
