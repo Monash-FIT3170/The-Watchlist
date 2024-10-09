2312804d1d267dc26b8fb8d2e2a867c881f7ca6c
mrud0004 <mrud0004@student.monash.edu>
2024-10-09 14:06:15 +1100

    Add loading in between top rated

--- imports/ui/pages/TopRated.jsx
+++ imports/ui/pages/TopRated.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect, useMemo} from 'react';
 import {Meteor} from 'meteor/meteor';
 import ProfileDropdown from '../components/profileDropdown/ProfileDropdown';
 import ContentItemDisplay from '../components/contentItems/ContentItemDisplay';
+import LoadingNoAnimation from './LoadingNoAnimation';
 
 const TopRated = ({currentUser}) => {
     const [isMovie, setIsMovie] = useState(true);   
@@ -22,6 +23,18 @@ const TopRated = ({currentUser}) => {
     }, []);
 
 
+    const handleTypeChange = (isItMovie) => {
+      setLoading(true);
+      setIsMovie(isItMovie);
+      
+
+      setTimeout(() => {
+        if (!loading) {
+          setLoading(false);
+        }
+      }, 2000);
+    };
+
 
     //if user is not logged in, redirect to login page.
     if (!currentUser) {
@@ -40,22 +53,34 @@ const TopRated = ({currentUser}) => {
               setTopRatedContent(result); 
               console.log(result); 
           }
+          setLoading(false);
       });
-
-      setLoading(false);
-
   }, [isMovie]);
 
 
   if (loading) {
-    return (
-      <div className="flex flex-col min-h-screen bg-darker">
-       <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
-       <div className="animate-spin rounded-full h-24 w-24 border-t-4 border-magenta"></div>
-          <p className="text-xl font-semibold">Loading the Top Rated Content</p>
-          <p className="text-gray-400 mt-2">Please wait a moment while we fetch your content.</p>
-        </div>
+    return(
+      <>
+    <div className="flex flex-col justify-end items-start w-full h-72 p-4 bg-gradient-to-tl from-zinc-900 via-zinc-700 to-zinc-600 rounded-t-lg shadow-md mb-4">
+      <div className="absolute top-4 right-6">
+        <ProfileDropdown user={currentUser} />
       </div>
+      <h1 className="text-7xl text-white font-bold mb-8">Top Rated Content</h1>
+      <div className="flex gap-4 mb-4">
+        <button
+          className={`text-lg text-white py-2 px-6 rounded-full shadow ${isMovie  ? 'bg-[#7B1450] text-white border-[#7B1450]' : 'bg-[#282525]'}`}
+        >
+          Movies
+        </button>
+        <button
+          className={`text-lg text-white py-2 px-6 rounded-full shadow ${isMovie === false  ? 'bg-[#7B1450] text-white border-[#7B1450]' : 'bg-[#282525]'} border-transparent border`}
+        >
+          TV Shows
+        </button>
+      </div>
+    </div>
+     <LoadingNoAnimation pageName="The Watchlist" pageDesc="Switching your Top Rated Content..." upperScreen={true}/>);
+     </>
     );
   }
 
@@ -70,13 +95,13 @@ const TopRated = ({currentUser}) => {
       <div className="flex gap-4 mb-4">
         <button
           className={`text-lg text-white py-2 px-6 rounded-full shadow ${isMovie  ? 'bg-[#7B1450] text-white border-[#7B1450]' : 'bg-[#282525]'}`}
-          onClick={() => setIsMovie(true)}
+          onClick={() => handleTypeChange(true)}
         >
           Movies
         </button>
         <button
           className={`text-lg text-white py-2 px-6 rounded-full shadow ${isMovie === false  ? 'bg-[#7B1450] text-white border-[#7B1450]' : 'bg-[#282525]'} border-transparent border`}
-          onClick={() => setIsMovie(false)}
+          onClick={() => handleTypeChange(false)}
         >
           TV Shows
         </button>
